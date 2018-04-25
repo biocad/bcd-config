d61baaba3b909ec1e77a9968a53adb42d12e02c4
Bogdan Neterebskii <bog2dan1@gmail.com>
2018-04-25 08:31:57 +0300

    version 0.1.0.3: `database` field to Mongo config added (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [0.1.0.3] - 2018-04-25
+### Added
+- Field `database` in Mongo config.
+
 ## [0.1.0.2] - 2018-03-19
 ### Added
 - Data for `bio-sources` library.
--- src/System/BCD/Config/Mongo.hs
+++ src/System/BCD/Config/Mongo.hs
@@ -5,13 +5,14 @@ module System.BCD.Config.Mongo
   , FromJsonConfig (..)
   ) where
 
-import           Data.Aeson.Picker   ((|--))
-import           System.BCD.Config   (FromJsonConfig (..), getConfigText)
+import           Data.Aeson.Picker ((|--))
+import           System.BCD.Config (FromJsonConfig (..), getConfigText)
 
 data MongoConfig = MongoConfig { _host     :: String
                                , _port     :: Int
                                , _user     :: String
                                , _password :: String
+                               , _database :: String
                                , _descr    :: String
                                }
   deriving (Show, Read, Eq)
@@ -24,4 +25,5 @@ instance FromJsonConfig MongoConfig where
                          (get "port")
                          (get "user")
                          (get "password")
+                         (get "database")
                          (get "descr")
